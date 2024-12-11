acc8114b7eb5f8392c9a5a26318778b8ee64c8aa
phil04786 <abmi04786@gmail.com>
2024-12-11 20:38:51 +0530

    new changes

--- redux-tut/src/containers/HomeContainer.js
+++ redux-tut/src/containers/HomeContainer.js
@@ -1,3 +1,14 @@
+import { connect } from "react-redux";
 import Home from "../components/Home";
 
-export default Home;
+import { addToCart } from "../Services/Actions/actions";
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+  addToCartHandler: (data) => dispatch(addToCart(data)), //transferring :- component(data) to dispatch(action)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps);
+
+// export default Home;
